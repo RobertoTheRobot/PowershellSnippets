4ab07ade99d232fbbd33ebf2d8305457738e3c18
Roberto <roberto.aldanas@booking.com>
2017-12-29 09:38:02 +0100

    correct spacing and indenting

--- activeDirectory.ps1
+++ activeDirectory.ps1
@@ -29,63 +29,46 @@ function Search-ByBitLockerKey {
 }
 
 function Get-LocalDC ($domain) {
-	
+    	
 	$ErrorActionPreference = 'SilentlyContinue'
 	
     if ($domain)
-     {
-     $DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext("Domain", $domain)
-     $Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
-     
-    
-	 $Socks = New-Object System.Net.Sockets.TCPClient
-	 # Try 389
-	 $Connect = $Socks.BeginConnect(($Domain.FindDomainController()).name,389,$null,$null)
-		
+    {
+        $DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext("Domain", $domain)
+        $Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+        
+        $Socks = New-Object System.Net.Sockets.TCPClient
+        # Try 389
+        $Connect = $Socks.BeginConnect(($Domain.FindDomainController()).name,389,$null,$null)
 	
-		$TTWait = $Connect.AsyncWaitHandle.WaitOne(250,$False)                      
-
+		$TTWait = $Connect.AsyncWaitHandle.WaitOne(250,$False) 
 		
-		If ($Socks.Connected) {
-		
+        If ($Socks.Connected) 
+        {		
 			return ($Domain.FindDomainController()).name
-
-		
 			$Null = $Socks.Close()
-	      }
-	   }
-         
-     
+	    }
+	}
 	else
     {
-    	$LocalDCs = ([System.DirectoryServices.ActiveDirectory.ActiveDirectorySite]::GetComputerSite()).Servers| ? { $_.Domain -match $domain }
+    	$LocalDCs = ([System.DirectoryServices.ActiveDirectory.ActiveDirectorySite]::GetComputerSite()).Servers| ? { $_.Domain -match $domain }    	
     	
+    	$potentials = @()    	
     	
-    	$potentials = @()
-    	
-    	
-    	ForEach ($LocalDC in $LocalDCs) {
-    		
-    		$Socks = New-Object System.Net.Sockets.TCPClient
-    		
-    		
+        ForEach ($LocalDC in $LocalDCs) 
+        {    		
+    		$Socks = New-Object System.Net.Sockets.TCPClient  
     		$Connect = $Socks.BeginConnect($LocalDC.Name,389,$null,$null)
+    		$TTWait = $Connect.AsyncWaitHandle.WaitOne(250,$False)     
     		
-    		
-    		$TTWait = $Connect.AsyncWaitHandle.WaitOne(250,$False)                      
-
-    		
-    		If ($Socks.Connected) {
-    		
+            If ($Socks.Connected) 
+            {    		
     			$potentials += $LocalDC.Name
-
     			$Null = $Socks.Close()
     		}
     	}
     	
-
-    	$DC = $potentials | Get-Random
-    	
+    	$DC = $potentials | Get-Random    	
 
     	Return $DC
     }
