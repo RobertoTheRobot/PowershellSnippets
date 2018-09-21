d506bf2f095c589c5f2ece21d8f384d88be6fff3
Roberto <roberto.perezaldanas@gmail.com>
2018-09-21 17:27:16 +0200

    Create encrypt_decrypt_using_certificate.ps1
    added encrypt_decrypt_using_certificate.ps1

---
+++ encrypt_decrypt_using_certificate.ps1
@@ -0,0 +1,50 @@
+
+Function Encrypt-Message {
+    param(
+     [Parameter(Position=0, Mandatory=$true, ValueFromPipeline = $true)] [string]$certificate_cn
+    ,[Parameter(Position=1, Mandatory=$true, ValueFromPipeline = $false)][string]$message
+    )
+
+    Begin
+    {
+        $Cert = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=$certificate_cn*"}
+    }
+    Process
+    {
+        $encoded_message = [system.text.encoding]::UTF8.GetBytes($message)
+        $encryptedBytes = $Cert.PublicKey.Key.Encrypt($encoded_message, $true)
+        $encrypted_message = [System.Convert]::ToBase64String($encryptedBytes)
+    }
+    End
+    {
+        return $encrypted_message
+    }
+}
+
+Function Decrypt-Message {
+    param(
+     [Parameter(Position=0, Mandatory=$true, ValueFromPipeline = $true)] [string]$certificate_cn
+    ,[Parameter(Position=1, Mandatory=$true, ValueFromPipeline = $false)][string]$message
+    )
+    Begin
+    {
+        $Cert = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -like "CN=$certificate_cn*"}
+    }
+    Process
+    {
+        $encryptedBytes = [System.Convert]::FromBase64String($message)
+        $decryptedBytes = $Cert.PrivateKey.Decrypt($encryptedBytes, $true)
+        $decrypted_message = [system.text.encoding]::UTF8.GetString($decryptedBytes)
+    }
+    End
+    {
+        return $decrypted_message
+    }
+}
+
+
+# TEST:
+# 
+# $a = Encrypt-Message -certificate_cn localhost -message "this is a test"
+# 
+# Decrypt-Message -certificate_cn localhost -message $a
