74abff958697c214858fc160fc45b5db2737b180
Roberto <roberto.perezaldanas@gmail.com>
2018-10-05 12:19:53 +0200

    Update Create-HashTable.ps1
    added new function

--- Create-HashTable.ps1
+++ Create-HashTable.ps1
@@ -1,6 +1,6 @@
 
 #
-# Usage: $my_collection | Create-HashTable -key 'prop1' -value 'prop2'
+# Usage: $my_collection | Out-HashTable -key 'prop1' -value 'prop2'
 #
 Function Out-HashTable {
     param(
@@ -26,6 +26,50 @@ Function Out-HashTable {
                 $hashTable[$item.($key)] = New-Object System.Collections.ArrayList
                 [void]$hashTable[$item.($key)].Add($item.($value))
             }
+        }
+    }
+    End
+    {
+        return $hashTable
+    }
+}
+
+
+#
+# Usage: $my_collection | TableTo-HashTable -key 'column1' -values @('column2','column3','column4')
+#
+Function TableTo-HashTable {
+    param(
+     [Parameter(Position=0, Mandatory=$true, ValueFromPipeline = $true)] [PSObject[]]$collection
+    ,[Parameter(Position=1, Mandatory=$true, ValueFromPipeline = $false)][string]$key
+    ,[Parameter(Position=2, Mandatory=$true, ValueFromPipeline = $false)][string[]]$values
+    )
+
+    Begin
+    {
+        $hashTable = @{}
+    }
+    Process
+    {
+        foreach($item in $collection)
+        {       
+            # If the key has value in this item
+            if($item.($key))
+            {   
+                # If the key has not been created yet in the hashtable, create it with a empty array value  
+                if(!$hashTable[$item.($key)])  
+                {
+                    $hashTable[$item.($key)] = New-Object System.Collections.ArrayList
+                } 
+                        
+                foreach($column in ($item.PSObject.Properties.Name | Where-Object { $_ -in $values}))
+                {  
+                    if($item.($column))
+                    {                            
+                        [void]$hashTable[$item.($key)].Add($item.($column))                            
+                    }
+                }  
+            }           
         }
     }
     End
