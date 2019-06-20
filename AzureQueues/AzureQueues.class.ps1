d3fdaa099d078bc626d47d3fe6a01f1b37e6365b
raldanas <roberto.aldanas@booking.com>
2019-06-20 11:20:37 +0200

    Added azure queue class and libraries

---
+++ AzureQueues/AzureQueues.class.ps1
@@ -0,0 +1,62 @@
+﻿using namespace Microsoft.Azure.Storage
+using namespace Microsoft.Azure.Storage.Queue
+
+Add-Type -Path .\lib\Microsoft.Azure.Storage.Common.dll
+Add-Type -Path .\lib\Microsoft.Azure.Storage.Queue.dll
+
+
+Class AzureQueuePS {
+
+    [string]$accountName
+    [string]$accountKey
+    [string]$queueName
+    [CloudQueue]$queue
+
+    #
+    # Constructor
+    #
+
+    AzureQueuePS([string]$accountName,[string]$accountKey,[string]$queueName) {
+
+        $this.accountName = $accountName
+        $this.accountKey  = $accountKey
+        $this.queueName   = $queueName
+
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=${accountName};AccountKey=${accountKey};EndpointSuffix=core.windows.net"
+
+        #Retrieve storage account from connection string.
+        $StorageAccount = [Microsoft.Azure.Storage.CloudStorageAccount]::Parse($connectionString)
+
+        $queueClient = [QueueAccountExtensions]::CreateCloudQueueClient($StorageAccount) 
+        $this.queue = $queueClient.GetQueueReference($queueName)
+        $this.queue.CreateIfNotExists();
+
+    }
+
+    #
+    # Methods
+    #
+
+    [string] GetMessage([bool]$deleteMessage) {
+        $message = $this.queue.GetMessage()
+
+        if($deleteMessage)
+        {
+            $this.queue.Delete($message)
+        }
+
+        return $message.AsString
+    }
+
+    [void] AddMessage([string]$stringMessage) {
+        $message = [CloudQueueMessage]::new($stringMessage)
+        $this.queue.AddMessage($message)
+    }
+
+}
+
+
+# Example:
+# $o = [AzureQueuePS]::new($accountName,$accountKey,$queueName)
+# $o.GetMessage($false)
+# $o.AddMessage("test")
