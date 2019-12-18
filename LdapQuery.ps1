0da28c0824259896fcadee4ec0b538d415c1bc64
raldanas <roberto.aldanas@booking.com>
2019-12-18 10:05:05 +0100

    more queries for ldap

--- LdapQuery.ps1
+++ LdapQuery.ps1
@@ -36,3 +36,23 @@ Function Get-LdapUserPosixGroups {
 }
 
 
+Function Get-LdapPosixGroupMembers {
+    param([string]$groupname)
+    $res = $null
+    $ldapPath = 'LDAP://XXXXXXXXXXXXXXX:636/dc=XXXXXX,dc=com'
+    $directoryEntry = [System.DirectoryServices.DirectoryEntry]::new($ldapPath,$null,$null,"FastBind");
+    $directorySearcher = New-Object System.DirectoryServices.DirectorySearcher($directoryEntry);
+    [Void]$directorySearcher.PropertiesToLoad.Add("*");
+    $directorySearcher.Filter = "((cn=$groupname))"
+    $res = $directorySearcher.FindAll() 
+    if($res.count -eq 0)
+    {
+        Write-Host "User not found" -ForegroundColor Yellow
+    }
+    else
+    {
+        Write-Host "Group found" -ForegroundColor Green
+        Write-Host "Members:"
+        $res | Foreach-Object { $_.Properties.memberuid.count }
+    }
+}
